6731f7e24938a140efe8ad16e98f158a300731eb
Joshua Johanan <josh359@gmail.com>
2012-09-07 16:27:49 -0400

    Finished the state abbr object. Created an SVG view object. Refactoring continues

--- JoshNS.js
+++ JoshNS.js
@@ -55,9 +55,100 @@
 	
 })(window.Josh = window.Josh || {}, window.jQuery);
 
+(function(Josh){})(window.Josh = window.Josh || {}, window.jQuery);
+
+(function(Josh){
+	var $svg;
+	
+	Josh.SVG = function(){
+		$svg = $('svg');
+	};
+	
+	Josh.SVG.prototype = {
+		addState: function(state){
+			$('#' + state).attr('class', 'white-state');
+		},
+		
+		clearAll: function(){
+			$('svg path, svg g').removeAttr('class');
+		}
+	};
+})(window.Josh = window.Josh || {}, window.jQuery);
+
 (function(Josh){
 	Josh.State = function(name){
 		this.name = name;
 		this.checkins = 1;
+	};
+})(window.Josh = window.Josh || {});
+
+(function(Josh){
+	var abbr = {
+		ALASKA: 		'AK',
+		ALABAMA: 		'AL',
+		ARKANSAS: 		'AR',
+		ARIZONA: 		'AZ',
+		CALIFORNIA: 	'CA',
+		COLORADO:		'CO',
+		CONNECTICUT:	'CT',
+		DELAWARE:		'DE',
+		FLORIDA:		'FL',
+		GEORGIA:		'GA',
+		HAWAII:			'HI',
+		IOWA:			'IA',
+		IDAHO:			'ID',
+		ILLINOIS:		'IL',
+		INDIANA: 		'IN',
+		KANSAS:			'KS',
+		KENTUCKY:		'KY',
+		LOUISIANA:		'LA',
+		MASSACHUSETTS:	'MA',
+		MARYLAND:		'MD',
+		MAINE:			'ME',
+		MINNESOTA:		'MN',
+		MISSOURI: 		'MO',
+		MISSISSIPPI:	'MS',
+		MONTANA:		'MT',
+		'NORTH CAROLINA':'NC',
+		'NORTH DAKOTA':	'ND',
+		NEBRASKA:		'NE',
+		'NEW HAMPSHIRE':'NH',
+		'NEW JERSEY':	'NJ',
+		'NEW MEXICO':	'NM',
+		NEVADA:			'NV',
+		'NEW YORK':		'NY',
+		OHIO:			'OH',
+		OKLAHOMA: 		'OK',
+		OREGON:			'OR',
+		PENNSYLVANIA:	'PA',
+		'RHODE ISLAND':	'RI',
+		'SOUTH CAROLINA':'SC',
+		'SOUTH DAKOTA':	'SD',
+		TENNESSEE:		'TN',
+		TEXAS: 			'TX',
+		UTAH:			'UT',
+		VIRGINIA:		'VA',
+		VERMONT:		'VT',
+		WASHINGTON:		'WA',
+		WISCONSIN:		'WI',
+		'WEST VIRGINIA':'WV',
+		WYOMING:		'WY'
+	};
+	
+	Josh.Place = function(lat, lng, state, name, date, year, type){
+		this.lat = lat;
+		this.lng = lng;
+		//need to check for abbreviation
+		if(state !== undefined){
+			if(state.length > 2){
+				this.state = abbr[state.toUpperCase()];
+			}else{
+				this.state = state.toUpperCase();
+			}
+		}
+		this.name = name;
+		this.date = date;
+		this.year = year;
+		this.type = type;
 	};
 })(window.Josh = window.Josh || {});
