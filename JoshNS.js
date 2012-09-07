de1b60cff2a1b0a89f2dcb38efdd717822fd55b7
Joshua Johanan <josh359@gmail.com>
2012-09-06 21:29:11 -0400

    I am grabbing something to eat and I don't know if I will be back so I need to commit

---
+++ JoshNS.js
@@ -0,0 +1,15 @@
+(function(Josh, $){
+	"use strict";
+	var states = {};
+	var checkins = [];
+	var years = {};
+	var fb = false;
+	var fs = false;
+	
+	Josh.US = function(){};
+	
+	Josh.US.prototype = {
+		
+	};
+	
+})(window.Josh = window.Josh || {}, window.jQuery)
