c0682a8faad0db59fe70ffcc1c80b399febb4ce2
Joshua Johanan <josh359@gmail.com>
2012-09-06 23:05:40 -0400

    message

--- JoshNS.js
+++ JoshNS.js
@@ -5,11 +5,59 @@
 	var years = {};
 	var fb = false;
 	var fs = false;
+	var $this;
 	
-	Josh.US = function(){};
-	
-	Josh.US.prototype = {
-		
+	Josh.US = function(){
+		$this = $(this);
 	};
 	
-})(window.Josh = window.Josh || {}, window.jQuery)
+	Josh.US.prototype = {
+		addCheckin: function(place){
+			checkins.push(place);
+			years[place.year] = (years[place.year] !== undefined) ? years[place.year] : {};
+			
+			if(states[place.state] === undefined){
+					states[place.state] = new Josh.State(place.state);
+					years[place.year][place.state] = new Josh.State(place.state);
+				}else{
+					states[place.state].checkins++;
+					if(years[place.year][place.state] === undefined){
+						years[place.year][place.state] = new Josh.State(place.state);
+					}else{
+						years[place.year][place.state].checkins++;
+					}
+				}
+			$this.trigger('checkin', place);
+		},
+		
+		sortedStates: function(states){
+			var stateArray = [];
+			
+			for(var i in states){
+				if(states[i].name !== undefined){
+					stateArray.push(states[i]);
+					stateArray.sort(function(a, b){return b.checkins - a.checkins});
+				}
+			}
+			return stateArray;
+		},
+		
+		getAll: function(){
+			return this.sortedStates(states);
+		},
+
+		getYear: function(year){
+			if(years[year] !== undefined){
+				return this.sortedStates(years[year]);
+			}
+		}
+	};
+	
+})(window.Josh = window.Josh || {}, window.jQuery);
+
+(function(Josh){
+	Josh.State = function(name){
+		this.name = name;
+		this.checkins = 1;
+	};
+})(window.Josh = window.Josh || {});
